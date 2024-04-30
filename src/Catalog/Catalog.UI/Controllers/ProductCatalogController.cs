2e92db6e968a6517f176bfe1994e27cc17bff285
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-30 09:44:12 +0500

    finish

--- src/Catalog/Catalog.UI/Controllers/ProductCatalogController.cs
+++ src/Catalog/Catalog.UI/Controllers/ProductCatalogController.cs
@@ -1,4 +1,7 @@
-﻿using Microsoft.AspNetCore.Http;
+﻿using Catalog.Application.UseCases.ProductCatalogs.Commands;
+using Catalog.Application.UseCases.ProductCatalogs.Queries;
+using MediatR;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.UI.Controllers
@@ -7,6 +10,54 @@ namespace Catalog.UI.Controllers
     [ApiController]
     public class ProductCatalogController : ControllerBase
     {
+        private readonly IMediator _mediator;
+
+        public ProductCatalogController(IMediator mediator)
+        {
+            _mediator = mediator;
+
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Create(CreateProductCatalogCommand command)
+        {
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
+
+        [HttpGet]
+        public async Task<IActionResult> GetAll(GetAllProductCatalogsQuery query)
+        {
+            var res = await _mediator.Send(query);
+            return Ok(res);
+        }
+
+        [HttpGet("{id}")]
+        public async Task<IActionResult> GetById(Guid id)
+        {
+            var res = await _mediator.Send(new GetProductCatalogByIdQuery()
+            {
+                Id = id
+            });
+            return Ok(res);
+        }
+
+        [HttpDelete("{id}")]
+        public async Task<IActionResult> Delete(Guid id)
+        {
+            var res = await _mediator.Send(new DeleteProductCatalogCommand()
+            {
+                Id = id
+            });
+            return Ok(res);
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> Update(UpdateProductCatalogCommand command)
+        {
+            var res = await _mediator.Send(command);
+            return Ok(res);
+        }
 
     }
 }
--- src/Catalog/Catalog.UI/Program.cs
+++ src/Catalog/Catalog.UI/Program.cs
@@ -1,4 +1,5 @@
-
+using MediatR;
+using System.Reflection;
 namespace Catalog.UI
 {
     public class Program
@@ -13,6 +14,7 @@ namespace Catalog.UI
             // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
             builder.Services.AddEndpointsApiExplorer();
             builder.Services.AddSwaggerGen();
+            builder.Services.AddMediatR(Assembly.GetExecutingAssembly());
 
             var app = builder.Build();
 
