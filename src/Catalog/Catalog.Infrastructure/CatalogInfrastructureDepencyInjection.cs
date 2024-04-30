decfa6e3e86f89b5102ea9b9e5e87ac954611be7
QosimovIbrohim <ibrohim.qosiomov@icloud.com>
2024-04-30 09:33:34 +0500

    Add project files.

---
+++ src/Catalog/Catalog.Infrastructure/CatalogInfrastructureDepencyInjection.cs
@@ -0,0 +1,21 @@
+﻿using Catalog.Infrastructure.Persistance;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Catalog.Infrastructure
+{
+    public static class CatalogInfrastructureDepencyInjection
+    {
+        public static IServiceCollection AddCatalogInfrastructureDepencyInjection(this IServiceCollection services, IConfiguration configuration)
+        {
+
+            services.AddDbContext<CatalogDbContext>(options =>
+            {
+                options.UseLazyLoadingProxies().UseNpgsql("Con");
+            });
+
+            return services;
+        }
+    }
+}
